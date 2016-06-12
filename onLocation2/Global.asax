f4a6a5993ea321ca3f5515351614e8255b045a2c
jdbogieman <justin.diblasi@gmail.com>
2016-06-12 12:39:17 -0500

    initial commit

---
+++ onLocation2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="onLocation2.MvcApplication" Language="C#" %>
---
+++ onLocation2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(onLocation2.Startup))]
+namespace onLocation2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
